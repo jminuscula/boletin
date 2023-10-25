e6907473e0198ec9c5e95aeddfc67a4fa5e40e1a
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2023-10-25 14:08:13 -0400

    test pipeline

--- boedb/boedb/tests/test_pipelines.py
+++ boedb/boedb/tests/test_pipelines.py
@@ -40,19 +40,25 @@ async def test_process_diario_boe_for_date_runs_pipeline():
     ArtPipelineMock = mock.AsyncMock()
 
     with (
-        mock.patch("boedb.pipelines.get_http_client_session", return_value=get_client_mock),
+        mock.patch(
+            "boedb.pipelines.get_http_client_session", return_value=get_client_mock
+        ),
         mock.patch(
             "boedb.pipelines.DiarioBoeSummaryExtractor",
         ) as SummaryExtractorMock,
         mock.patch(
             "boedb.pipelines.DiarioBoeArticlesExtractor",
         ) as ArticlesExtractorMock,
-        mock.patch("boedb.pipelines.Pipeline", side_effect=[SumPipelineMock, ArtPipelineMock]) as PipelineMock,
+        mock.patch(
+            "boedb.pipelines.Pipeline", side_effect=[SumPipelineMock, ArtPipelineMock]
+        ) as PipelineMock,
     ):
         await process_diario_boe_for_date(dt)
 
         SummaryExtractorMock.assert_called_once_with(dt, client_mock)
         SumPipelineMock.run.assert_awaited_once()
 
-        ArticlesExtractorMock.assert_called_once_with(summary_mock, client_mock)
+        ArticlesExtractorMock.assert_called_once_with(
+            summary_mock, client_mock, batch_size=10
+        )
         ArtPipelineMock.run.assert_awaited_once()
