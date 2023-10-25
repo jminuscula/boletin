fa46f6c0b8019f35aa5ceb96c66965b4790863a9
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2023-10-25 13:57:15 -0400

    config management

---
+++ boedb/boedb/config.py
@@ -0,0 +1,29 @@
+import os
+from dataclasses import dataclass
+
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(".env.shared"),  # common configurable settings
+    **dotenv_values(".env.secret"),  # API keys
+    **os.environ,  # environment overrides
+}
+
+
+@dataclass
+class DiarioBoeConfig:
+    # Number of concurrent requests to extract Article data from boe.es
+    ARTICLE_EXTRACT_BATCH_SIZE = 10
+
+    # Number of articles to be processed concurrently, including
+    # LLM processing through OpenAi's API
+    ARTICLE_TRANSFORM_BATCH_SIZE = 10
+
+    # We can't exceed LLM's max context tokens, so the original text
+    # plus the generated outcome must be controlled
+    ARTICLE_FRAGMENT_MAX_LENGTH = 8192
+
+
+@dataclass
+class OpenAiConfig:
+    API_KEY = config["OPENAI_API_KEY"]
