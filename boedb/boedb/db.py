745f457528d27126e23a2e46b88ffa1d4d13a593
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2023-11-06 11:52:47 -0500

    db_client based loaders

--- boedb/boedb/db.py
+++ boedb/boedb/db.py
@@ -1,6 +1,7 @@
 import itertools
 
 import psycopg
+from psycopg_pool import AsyncConnectionPool, ConnectionPool
 
 from boedb.config import DBConfig
 
@@ -17,8 +18,10 @@ def get_db_client():
 class PostgresClient:
     def __init__(self, dsn):
         self.dsn = dsn
+        self.pool = ConnectionPool(dsn)
+        self.async_pool = AsyncConnectionPool(dsn)
 
-    def insert_many(self, row_dicts, table, columns=None):
+    def insert_many(self, table, row_dicts, columns=None):
         if columns is None:
             columns = set(itertools.chain.from_iterable(list(rd.keys()) for rd in row_dicts))
 
@@ -29,16 +32,21 @@ class PostgresClient:
 
         with psycopg.connect(self.dsn) as conn:  # pylint: disable-all
             with conn.cursor() as cursor:
-                columns_fmt = ", ".join(self.columns)
-                values_fmt = ", ".join(r"%s" for c in self.columns)
-                sql = f"INSERT INTO {self.table} ({columns_fmt}) VALUES ({values_fmt})"
+                columns_fmt = ", ".join(columns)
+                values_fmt = ", ".join(r"%s" for c in columns)
+                sql = f"INSERT INTO {table} ({columns_fmt}) VALUES ({values_fmt})"
 
                 return cursor.executemany(sql, rows)
 
-    def insert(self, table, row_dict):
-        return self.insert_many(table, [row_dict])
+    def insert(self, table, row_dict, columns=None):
+        return self.insert_many(table, [row_dict], columns)
 
     def execute(self, sql):
-        with psycopg.connect(self.dsn) as conn:  # pylint: disable-all
+        with self.pool.connection() as conn:  # pylint: disable-all
             with conn.cursor() as cursor:
                 yield from cursor.execute(sql)
+
+    async def execute_async(self, sql):
+        async with self.async_pool.connection() as conn:  # pylint: disable-all
+            async with conn.cursor() as cursor:
+                return await cursor.execute(sql)
