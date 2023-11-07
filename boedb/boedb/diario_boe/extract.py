e8a1a246281c86ed045538fd1aa34ec512f5e4b1
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2023-11-07 16:42:36 -0500

    httpclient based extractors

--- boedb/boedb/diario_boe/extract.py
+++ boedb/boedb/diario_boe/extract.py
@@ -1,5 +1,6 @@
 from xml.etree import ElementTree
 
+from boedb.client import HttpClient
 from boedb.config import get_logger
 from boedb.diario_boe.models import Article, DaySummary
 from boedb.pipelines.step import BaseStepExtractor
@@ -8,34 +9,32 @@ from boedb.pipelines.stream import StreamPipelineBaseExecutor
 BASE_URL = "https://www.boe.es"
 
 
-async def extract_boe_xml(doc_id, session):
-    url = f"{BASE_URL}/diario_boe/xml.php"
-    async with session.get(url, params={"id": doc_id}) as resp:
-        resp.raise_for_status()
-        xml = await resp.text()
+async def extract_boe_xml(doc_id, client):
+    url = f"{BASE_URL}/diario_boe/xml.php?id={doc_id}"
+    xml = await client.get(url, parse_response=False)
     return ElementTree.fromstring(xml)
 
 
-async def extract_boe_summary(summary_id, session):
-    xml = await extract_boe_xml(summary_id, session)
+async def extract_boe_summary(summary_id, client):
+    xml = await extract_boe_xml(summary_id, client)
     return DaySummary.from_xml(xml)
 
 
-async def extract_boe_article(article_id, summary_id, session):
-    xml = await extract_boe_xml(article_id, session)
+async def extract_boe_article(article_id, summary_id, client):
+    xml = await extract_boe_xml(article_id, client)
     return Article.from_xml(xml, summary_id)
 
 
 class SummaryExtractor(BaseStepExtractor):
     def __init__(self, date, http_session, should_skip=None):
         self.date = date
-        self.http_session = http_session
         self.should_skip = should_skip
+        self.client = HttpClient(http_session)
         self.logger = get_logger("boedb.diario_boe.summary_extractor")
 
     async def __call__(self):
         summary_id = f"BOE-S-{self.date.strftime('%Y%m%d')}"
-        doc = await extract_boe_summary(summary_id, self.http_session)
+        doc = await extract_boe_summary(summary_id, self.client)
 
         if self.should_skip is not None and self.should_skip(doc):
             self.logger.info(f"Skipping {doc.summary_id}")
@@ -48,7 +47,7 @@ class SummaryExtractor(BaseStepExtractor):
 class ArticlesExtractor(StreamPipelineBaseExecutor):
     def __init__(self, concurrency, http_session, should_skip=None):
         self.logger = get_logger("boedb.diario_boe.article_extractor")
-        self.http_session = http_session
+        self.client = HttpClient(http_session)
         self.should_skip = should_skip
         super().__init__(concurrency)
 
@@ -57,7 +56,7 @@ class ArticlesExtractor(StreamPipelineBaseExecutor):
             self.logger.debug(f"Skipping {item}")
             return
 
-        doc = await extract_boe_article(item.entry_id, item.summary_id, self.http_session)
+        doc = await extract_boe_article(item.entry_id, item.summary_id, self.client)
 
         fragments = doc.split()
         self.logger.debug(f"Extracted {doc}")
--- boedb/boedb/diario_boe/tests/test_extract.py
+++ boedb/boedb/diario_boe/tests/test_extract.py
@@ -7,82 +7,135 @@ from aioresponses import aioresponses
 
 from boedb.client import get_http_client_session
 from boedb.diario_boe.extract import (
-    DiarioBoeArticlesExtractor,
-    DiarioBoeSummaryExtractor,
+    ArticlesExtractor,
+    SummaryExtractor,
     extract_boe_article,
     extract_boe_summary,
     extract_boe_xml,
 )
-from boedb.diario_boe.models import Article, DaySummary
+from boedb.diario_boe.models import Article, ArticleFragment, DaySummary, DaySummaryEntry
 
 
 @pytest.mark.asyncio
 async def test_extract_boe_xml_makes_request_and_returns_root():
-    base_url = "https://www.boe.es/diario_boe/xml.php"
+    url = "https://www.boe.es/diario_boe/xml.php?id=doc_id"
     xml_doc = b"<xml><test /></xml>"
-    expected_url = f"{base_url}?id=doc_id"
-    with aioresponses() as mock_server:
-        mock_server.get(expected_url, status=200, body=xml_doc)
-        async with get_http_client_session() as client:
-            root = await extract_boe_xml("doc_id", client)
-            mock_server.assert_called_once_with(
-                base_url, **{"headers": {}, "trace_request_ctx": {"current_attempt": 1}}, params={"id": "doc_id"}
-            )
-            assert ElementTree.tostring(root) == xml_doc
+    client_mock = mock.Mock()
+    with mock.patch("boedb.diario_boe.extract.HttpClient") as HttpClientMock:
+        HttpClientMock.return_value = client_mock
+        client_mock.get = mock.AsyncMock(return_value=xml_doc)
+        root = await extract_boe_xml("doc_id", client_mock)
+
+    client_mock.get.assert_awaited_once_with(url, parse_response=False)
+    assert ElementTree.tostring(root) == xml_doc
+
+
+@pytest.mark.asyncio
+async def test_extract_boe_summary_extracts_and_creates_daysummary():
+    summary_id = "summary_id"
+    root = mock.Mock()
+    client_mock = mock.Mock()
+
+    with mock.patch(
+        "boedb.diario_boe.extract.extract_boe_xml", return_value=mock.AsyncMock()
+    ) as extract_mock, mock.patch("boedb.diario_boe.extract.DaySummary") as DaySummaryMock:
+        extract_mock.return_value = root
+
+        await extract_boe_summary(summary_id, client_mock)
+
+    extract_mock.assert_awaited_once_with(summary_id, client_mock)
+    DaySummaryMock.from_xml.assert_called_once_with(root)
+
+
+@pytest.mark.asyncio
+async def test_extract_boe_article_extracts_and_creates_article():
+    article_id, summary_id = "article_id", "summary_id"
+    root = mock.Mock()
+    client_mock = mock.Mock()
+
+    with mock.patch(
+        "boedb.diario_boe.extract.extract_boe_xml", return_value=mock.AsyncMock()
+    ) as extract_mock, mock.patch("boedb.diario_boe.extract.Article") as ArticleMock:
+        extract_mock.return_value = root
+
+        await extract_boe_article(article_id, summary_id, client_mock)
+
+    extract_mock.assert_awaited_once_with(article_id, client_mock)
+    ArticleMock.from_xml.assert_called_once_with(root, summary_id)
 
 
 @pytest.mark.asyncio
 async def test_summary_extractor_extracts_doc_for_date():
     date = datetime(2023, 9, 14)
     summary_id = "BOE-S-20230914"
-    mock_client = mock.Mock()
+    session_mock = mock.Mock()
+    client_mock = mock.AsyncMock()
     extracted = mock.Mock()
-    with mock.patch("boedb.diario_boe.extract.extract_boe_summary", return_value=extracted) as extract_mock:
-        extractor = DiarioBoeSummaryExtractor(date, mock_client)
+    with mock.patch(
+        "boedb.diario_boe.extract.extract_boe_summary", return_value=extracted
+    ) as extract_mock, mock.patch("boedb.diario_boe.extract.HttpClient") as HttpClientMock:
+        HttpClientMock.return_value = client_mock
+        extractor = SummaryExtractor(date, session_mock)
 
         result = await extractor()
         assert result is extracted
-        extract_mock.assert_awaited_once_with(summary_id, mock_client)
+
+        extract_mock.assert_awaited_once_with(summary_id, client_mock)
+        HttpClientMock.assert_called_once_with(session_mock)
 
 
 @pytest.mark.asyncio
-async def test_articles_extractor_extracts_items_from_summary():
-    articles = [Article, Article]
-    summary = mock.Mock(spec=DaySummary, items=articles)
+async def test_summary_extractor_skips_item_when_should_skip():
+    date = datetime(2023, 9, 14)
+    summary_id = "BOE-S-20230914"
+    session_mock = mock.Mock()
+    summary_mock = mock.Mock(spec=DaySummary, summary_id=summary_id)
+    should_skip = mock.Mock(return_value=True)
 
-    async with get_http_client_session() as client:
-        extractor = DiarioBoeArticlesExtractor(summary, client)
-        items = await extractor.gather()
-        assert items == articles
+    with mock.patch("boedb.diario_boe.extract.extract_boe_summary", return_value=summary_mock):
+        extractor = SummaryExtractor(date, session_mock, should_skip)
+        extracted = await extractor()
 
-
-@pytest.mark.asyncio
-async def test_articles_extractor_extracts_items_in_process():
-    articles = [Article, Article]
-    summary = mock.Mock(spec=DaySummary, summary_id="summary_id", items=articles)
-    item = mock.Mock(entry_id=1)
-    extracted = mock.Mock()
-
-    with mock.patch("boedb.diario_boe.extract.extract_boe_article", return_value=extracted) as extract_mock:
-        async with get_http_client_session() as client:
-            extractor = DiarioBoeArticlesExtractor(summary, client)
-            processed = await extractor.process(item)
-
-            extract_mock.assert_awaited_once_with(item.entry_id, summary.summary_id, client)
-            assert processed == extracted
+    should_skip.assert_called_once_with(summary_mock)
+    assert extracted is None
 
 
 @pytest.mark.asyncio
-async def test_articles_extractor_splits_articles():
-    article = mock.Mock(spec=Article)
-    subarticle = mock.Mock(spec=Article)()
-    article.split.return_value = [subarticle, subarticle]
-    summary = mock.Mock(spec=DaySummary, items=[article])
+async def test_articles_extractor_process_extracts_items_from_entry():
+    session_mock = mock.Mock()
+    client_mock = mock.AsyncMock()
 
-    async with get_http_client_session() as client:
-        extractor = DiarioBoeArticlesExtractor(summary, client)
-        extractor.process = mock.AsyncMock(side_effect=lambda i: i)
+    summary_id, entry_id = "summary_id", "entry_id"
+    entry = DaySummaryEntry(summary_id, entry_id)
 
-        result = await extractor()
-        extractor.process.assert_called_with(article)
-        assert result == [subarticle, subarticle]
+    article_mock = mock.Mock(spec=Article)
+    fragment_mock = mock.Mock(spec=ArticleFragment)
+    article_mock.split.return_value = [fragment_mock]
+
+    with mock.patch("boedb.diario_boe.extract.extract_boe_article") as extract_mock, mock.patch(
+        "boedb.diario_boe.extract.HttpClient"
+    ) as HttpClientMock:
+        extract_mock.return_value = article_mock
+        HttpClientMock.return_value = client_mock
+
+        extractor = ArticlesExtractor(1, session_mock)
+        result = await extractor.process(entry)
+
+    extract_mock.assert_awaited_once_with(entry.entry_id, entry.summary_id, client_mock)
+    assert result == [article_mock, fragment_mock]
+
+
+@pytest.mark.asyncio
+async def test_article_extractor_skips_item_when_should_skip():
+    session_mock = mock.Mock()
+    client_mock = mock.AsyncMock()
+
+    summary_id, entry_id = "summary_id", "entry_id"
+    entry = DaySummaryEntry(summary_id, entry_id)
+    should_skip = mock.Mock(return_value=True)
+
+    extractor = ArticlesExtractor(1, session_mock, should_skip)
+    extracted = await extractor.process(entry)
+
+    should_skip.assert_called_once_with(entry)
+    assert extracted is None
